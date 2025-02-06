0f6a02ebb29f5371c2120b48ad9583bc5e4d456b
anrybalka <ribalkaandrey666@gmail.com>
2025-02-06 16:30:23 +0500

    upd

---
+++ 007_selenium/tests/conftest.py
@@ -0,0 +1,27 @@
+"""
+Fixture
+"""
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="session")
+def browser():
+    """
+    Main fixture
+    """
+    # оптимизация запуска Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+
+    # установить свежий драйвер прослойку между кодом и браузером
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service, options=chrome_options)
+    yield browser
+    browser.quit()
--- 007_selenium/tests/test_001_main.py
+++ 007_selenium/tests/test_001_main.py
@@ -1,14 +1,17 @@
 """
 2025 (C) anrybalka
 """
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-def test_smoke():
+def test_smoke(browser):
     """
     TEST-ID. Smoke test
     """
-    driver = webdriver.Chrome()
-    driver.get("http://selenium.dev")
-    driver.quit()
-
-    assert True, ""
+    browser.get("https://postcard.qa.studio/")
+    button = browser.find_element(by=By.ID, value="send")
+    assert button.text == "Отправить", f"Unexpected text on button {button.text}"
