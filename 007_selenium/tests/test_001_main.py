ffafba89d43cdcf03423a97ae900f00ac1c0d6dd
anrybalka <ribalkaandrey666@gmail.com>
2025-02-07 00:02:33 +0500

    upd

--- 007_selenium/tests/test_001_main.py
+++ 007_selenium/tests/test_001_main.py
@@ -8,10 +8,61 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-def test_smoke(browser):
+URL = "https://postcard.qa.studio/"
+
+def test_button(browser):
     """
-    TEST-ID. Smoke test
+    TEST-1. button test
     """
-    browser.get("https://postcard.qa.studio/")
+    browser.get(URL)
     button = browser.find_element(by=By.ID, value="send")
     assert button.text == "Отправить", f"Unexpected text on button {button.text}"
+
+def test_class(browser):
+    """
+    TEST-2. class test
+    """
+    browser.get(URL)
+    
+    email_lable = browser.find_element(by=By.CSS_SELECTOR, value="div.email h2")
+    email_lable_class = email_lable.get_attribute("class")
+
+    assert email_lable_class == "requered", f"Unexpected text {button.text}"
+
+    button = browser.find_element(by=By.ID, value="send")
+    button.click()
+
+    email_lable = browser.find_element(by=By.CSS_SELECTOR, value="div.email h2")
+    email_lable_class = email_lable.get_attribute("class")
+
+    assert email_lable_class == "requered error", f"Unexpected text {button.text}"
+
+
+CASE = [0, 1]
+@pytest.mark.parametrize('case', CASE)
+def test_send_postcard(browser, case):
+    """
+    TEST-3. test_send_postcard
+    """
+    browser.get(URL)
+
+    email = browser.find_element(by=By.ID, value="email")
+    email.click()
+    email.send_keys("tocinid643@shouxs.com")
+
+    textarea = browser.find_element(by=By.ID, value="textarea")
+    textarea.click()
+    textarea.send_keys("TEST-3. test_send_postcard")
+
+    images = browser.find_elements(by=By.CLASS_NAME, value="photo-input__photo")
+
+    image1 = images[case]
+    image1.click()
+
+    button = browser.find_element(by=By.ID, value="send")
+    button.click()
+
+    ok_message = browser.find_element(by=By.CSS_SELECTOR, value="#modal div h3")
+    ok_message_text = ok_message.text
+
+    assert ok_message_text == "Открытка успешно отправлена!", "Ошибка с отправкой открытки"
